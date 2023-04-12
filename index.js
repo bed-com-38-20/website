c9240a86aa0960af5968478aa8f0ba9b79fa29af
bed-com-38-20 <bed-com-38-20@unima.ac.mw>
2023-04-12 16:20:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const dbConnection = require('./src/utils/mysql.connector')
+
+const { Post } = require('./src/posts/post.model')
+
+// app.get('/api/v1', function (req, res) {
+//     return res.json(req.headers)
+// })
+
+app.get('/api/v1/posts', function (req, res) {
+    var sql2 = "SElECT * FROM posts"
+    return dbConnection.query(sql2, function (err, result) {
+        if (err) throw err;
+
+        return res.json(result)
+    })
+})
+
+// Create new post/article in the database
+app.post('/api/v1/posts', function (req, res) {
+    // console.log(req.body)
+    const { name, imageUrl, summary } = req.body // destructure sent properties from the REQUEST body
+
+    // construct sql query
+    var sql = `INSERT INTO posts (name, imageUrl, summary) VALUES ('${name}','${imageUrl}','${summary}')`;
+    // console.log(sql)
+    // Query the MySQL database and return result to the client app ie. POSTMAN or Web APP
+    return dbConnection.query(sql, function (err, result) {
+        if (err) throw err; // if error throw it, else continue execution to next line
+        return res.json(result)
+        // console.log("1 record inserted");
+    });
+    // res.json(sql)
+})
+
+app.listen(3000, function () {
+    console.log('ARTISAN listening on port 3000')
+
+    dbConnection.connect(function (err) {
+        if (err) throw err
+
+        console.log("Connected to MySQL")
+    })
+})
---
+++ src/POST/post.moldel.js
@@ -0,0 +1,16 @@
+const post = {
+    id: "",
+    creator: "",
+    comments: "",
+    name: "",
+    imageUrl: "",
+    summary: ""
+}
+
+class Post {
+    constructor() {
+
+    }
+}
+
+module.exports = { Post }
